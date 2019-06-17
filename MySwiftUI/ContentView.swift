9b279a78a4e87a88eeedf0ae3458ba2d4f40fafd
Wasith Theerapattrathamrong <phai@live.com>
2019-06-17 13:45:17 +0700

    Initial Commit

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by Wasith Theerapattrathamrong on 17/6/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
